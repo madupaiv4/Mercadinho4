028f4087b708303baff6a3dda6490c2090479b0e
Gab <gabyrrmaciel@gmail.com>
2025-11-15 15:28:02 -0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Cupom.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Cupom
+    {
+        static int Numero;
+        public DateTime Emissao;
+        public string Cliente;
+        public List<ItemCupom> ItensCupom { get; set; } = new List<ItemCupom>();
+        public int PCupom;
+
+
+
+
+        public Cupom(DateTime emissao, string cliente, List<ItemCupom> itensCupom, int pCupom)
+        {
+            Emissao = emissao;
+            Cliente = cliente;
+            ItensCupom = itensCupom;
+            PCupom = pCupom;
+        }
+        public Cupom(int numero, DateTime emissao, string cliente, List<ItemCupom> itensCupom)
+        {
+
+            Numero = numero; // numero cupom
+            Emissao = DateTime.Now;
+            Console.WriteLine(Emissao);
+            Console.WriteLine("Digite seu CPF:");
+            Cliente = Console.ReadLine();
+            ItensCupom = itensCupom;
+            PCupom = ObterProxCupom(Numero);
+
+        }
+
+        private int ObterProxCupom(in int numero)
+        {
+            PCupom = numero + 1;
+            return PCupom;
+        }
+
+        public Cupom(int numero, DateTime emissao, string cliente, int pcupom)
+        {
+            Numero = numero;
+            Emissao = emissao;
+            Cliente = cliente.ToUpper();
+            PCupom = pcupom;
+        }
+
+        public Cupom()
+        {
+        }
+
+
+        public int CalcularValorTotal(ItemCupom itemCupom)
+        {
+            int x = itemCupom.CalcularValorItem();
+            return x;
+        }
+
+
+
+
+        public class ItemCupom
+        {
+            public int Numero;
+            public Produto item;
+            public int quantidade;
+
+
+
+            public ItemCupom(int numero, Produto item, int quantidade)
+            {
+                numero = Numero;
+                this.item = item;
+                item.Preco = item.Preco;
+                this.quantidade = quantidade;
+            }
+            public int CalcularValorItem()
+            {
+                int x = this.quantidade * item.Preco;
+
+                return x;
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Produto
+    {
+        public static Random random = new Random();
+        public static string Codigo;
+        public string Descricao;
+        public string Unidade;
+        public int Preco;
+        public float SaldoEstoque;
+
+        public Produto(string codigo, string descricao, string unidade, int preco, float saldoEstoque)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+            Unidade = unidade;
+            Preco = preco;
+            SaldoEstoque = saldoEstoque;
+        }
+
+        public Produto()
+        {
+            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Codigo = GenerateCodigo(8);
+            Descricao = $"Produto {random.Next(1, 10000)}";
+            Unidade = PickUnidade();
+            SaldoEstoque = (float)Math.Round(random.NextDouble() * 1000.0, 2); // estoque entre 0 e 1000
+            Preco = random.Next(1, 10000); // preço entre 0.00 e 500.00
+        }
+
+        private static string GenerateCodigo(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var sb = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+                sb.Append(chars[random.Next(chars.Length)]);
+            return sb.ToString();
+        }
+
+        private static string PickUnidade()
+        {
+            string[] unidades = { "un", "kg", "l", "cx", "m", "pc" };
+            return unidades[random.Next(unidades.Length)];
+        }
+    }
+}
